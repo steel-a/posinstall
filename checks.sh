0636d9317a91616adc01eea8e02c392a54768bc0
steel-a <64929461+steel-a@users.noreply.github.com>
2025-08-29 17:37:01 -0300

    Update checks.sh

--- checks.sh
+++ checks.sh
@@ -3,6 +3,21 @@
 RESOURCE="$1"
 
 case "$RESOURCE" in
+  i3)
+    command -v i3 >/dev/null 2>&1 &&
+    command -v picom >/dev/null 2>&1 &&
+    command -v polybar >/dev/null 2>&1 &&
+    command -v rofi >/dev/null 2>&1 &&
+    command -v dunst >/dev/null 2>&1 &&
+    command -v feh >/dev/null 2>&1 &&
+    command -v flameshot >/dev/null 2>&1 &&
+    command -v lxappearance >/dev/null 2>&1 &&
+    command -v nm-connection-editor >/dev/null 2>&1 &&
+    command -v xclip >/dev/null 2>&1 &&
+    command -v xfce4-power-manager >/dev/null 2>&1 &&
+    command -v dmenu >/dev/null 2>&1 &&
+    command -v slock >/dev/null 2>&1
+    ;;
   hyprland) exit 0 ;;
   docker) command -v docker >/dev/null 2>&1 && docker info >/dev/null 2>&1 ;;
   "Teste 04") exit 0 ;;
