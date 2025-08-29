191338a9ccb7a4e20ab3420f6d5254104de206d1
steel-a <64929461+steel-a@users.noreply.github.com>
2025-08-29 19:24:09 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -128,7 +128,7 @@ while true; do
   if script_exists "$install_script"; then
     echo ""
     echo "🔧 Instalando $opcao..."
-    bash <(curl -SL "$install_script")
+    bash <(curl -sSL "$install_script")
   else
     echo "❌ Opção inválida ou script não disponível."
   fi
