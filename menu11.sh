93e1e9ea9e3a54645fd13154bd3ff8b441a3fbe6
steel-a <64929461+steel-a@users.noreply.github.com>
2025-08-29 16:05:40 -0300

    Update and rename menu10.sh to menu11.sh

--- menu11.sh
+++ menu11.sh
@@ -102,7 +102,7 @@ while true; do
   )
 
 
-  opcao=$(echo "$selected" | sed -E 's/^(\[[x ]\]|❌  ) //')
+  opcao=$(echo "$selected" | sed -E 's/^\s*(\[[x ]\]|❌|-) +//')
   
 
   if [[ "$opcao" == "Sair" ]]; then
